292e29054fb7aa34e29980fb07dd09074a43b343
¨kaviyachandran¨ <kaviya.dhanabalachandran@gmail.com>
2023-10-20 13:10:07 +0200

    add velocity mujoco config

--- scripts/iai_robots/pr2/pr2_mujoco.py
+++ scripts/iai_robots/pr2/pr2_mujoco.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 import rospy
 
-from giskardpy.configs.behavior_tree_config import OpenLoopBTConfig
+from giskardpy.configs.behavior_tree_config import ClosedLoopBTConfig
 from giskardpy.configs.giskard import Giskard
-from giskardpy.configs.iai_robots.pr2 import PR2CollisionAvoidance, PR2JointTrajServerMujocoInterface, \
+from giskardpy.configs.iai_robots.pr2 import PR2CollisionAvoidance, PR2VelocityMujocoInterface, \
     WorldWithPR2Config
 from giskardpy.configs.qp_controller_config import QPControllerConfig
 
 if __name__ == '__main__':
     rospy.init_node('giskard')
     giskard = Giskard(world_config=WorldWithPR2Config(),
-                      collision_avoidance_config=PR2CollisionAvoidance(),
-                      robot_interface_config=PR2JointTrajServerMujocoInterface(),
-                      behavior_tree_config=OpenLoopBTConfig(),
-                      qp_controller_config=QPControllerConfig())
+                    collision_avoidance_config=PR2CollisionAvoidance(),
+                    robot_interface_config=PR2VelocityMujocoInterface(),
+                    behavior_tree_config=ClosedLoopBTConfig(),
+                    qp_controller_config=QPControllerConfig())
     giskard.live()
