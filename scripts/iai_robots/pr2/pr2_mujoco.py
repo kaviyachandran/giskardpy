1a6ecab296b2c076283f6f75b8cf9f9d94157819
¨kaviyachandran¨ <kaviya.dhanabalachandran@gmail.com>
2023-10-26 19:35:13 +0200

    Use velocity controller

--- scripts/iai_robots/pr2/pr2_mujoco.py
+++ scripts/iai_robots/pr2/pr2_mujoco.py
@@ -15,3 +15,22 @@ if __name__ == '__main__':
                     behavior_tree_config=ClosedLoopBTConfig(),
                     qp_controller_config=QPControllerConfig())
     giskard.live()
+
+
+# #!/usr/bin/env python
+# import rospy
+
+# from giskardpy.configs.behavior_tree_config import OpenLoopBTConfig
+# from giskardpy.configs.giskard import Giskard
+# from giskardpy.configs.iai_robots.pr2 import PR2CollisionAvoidance, PR2JointTrajServerMujocoInterface, \
+#     WorldWithPR2Config
+# from giskardpy.configs.qp_controller_config import QPControllerConfig
+
+# if __name__ == '__main__':
+#     rospy.init_node('giskard')
+#     giskard = Giskard(world_config=WorldWithPR2Config(),
+#                       collision_avoidance_config=PR2CollisionAvoidance(),
+#                       robot_interface_config=PR2JointTrajServerMujocoInterface(),
+#                       behavior_tree_config=OpenLoopBTConfig(),
+#                       qp_controller_config=QPControllerConfig())
+#     giskard.live()
