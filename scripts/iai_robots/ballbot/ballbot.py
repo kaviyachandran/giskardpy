2f9836204e8b7d26b3c58916c0fa7fc0f45dd377
¨kaviyachandran¨ <kaviya.dhanabalachandran@gmail.com>
2024-06-12 12:33:49 +0200

    Add object with freejoint as robot

---
+++ scripts/iai_robots/ballbot/ballbot.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import rospy
+
+from giskardpy.configs.giskard import Giskard
+from giskardpy.configs.iai_robots.ballbot import BallBotWorldConfig, BallBotCollisionAvoidanceConfig, \
+    BallBotJointTrajServerMujocoInterface
+
+if __name__ == '__main__':
+    rospy.init_node('giskard')
+    giskard = Giskard(world_config=BallBotWorldConfig(),
+                      collision_avoidance_config=BallBotCollisionAvoidanceConfig(),
+                      robot_interface_config=BallBotJointTrajServerMujocoInterface())
---
+++ src/giskardpy/configs/iai_robots/ballbot.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+from giskardpy.configs.collision_avoidance_config import DisableCollisionAvoidanceConfig
+from giskardpy.configs.robot_interface_config import RobotInterfaceConfig, StandAloneRobotInterfaceConfig
+from giskardpy.configs.world_config import WorldWithFixedRobot
+from giskardpy.my_types import Derivatives
+
+
+class BallBotWorldConfig(WorldWithFixedRobot):
+    def __init__(self):
+        super().__init__({Derivatives.velocity: 0.2,
+                          Derivatives.acceleration: np.inf,
+                          Derivatives.jerk: 15})
+
+
+class BallBotCollisionAvoidanceConfig(DisableCollisionAvoidanceConfig):
+    def __init__(self):
+        #  Todo: Check if ballbot needs a collision matrix
+        super().__init__()
+
+
+class BallBotJointTrajServerMujocoInterface(RobotInterfaceConfig):
+    def setup(self):
+        self.sync_joint_state_topic('/joint_states')
+        self.add_follow_joint_trajectory_server(
+            namespace='/ballbot/follow_joint_trajectory',
+            state_topic='/ballbot/follow_joint_trajectory/state')
+
+
+
+class BallBotStandAloneRobotInterfaceConfig(StandAloneRobotInterfaceConfig):
+    def __init__(self):
+        super().__init__([
+            'transx_joint',
+            'transy_joint',
+            'transz_joint',
+            'rotx_joint',
+            'roty_joint',
+            'rotz_joint'
+        ])
