fc8bc55c02e8d89767db1c6eb484b35262c9dce1
¨kaviyachandran¨ <kaviya.dhanabalachandran@gmail.com>
2024-06-13 17:58:35 +0200

    hack from Simon to fix controlling fixed robots

--- src/giskardpy/configs/iai_robots/ballbot.py
+++ src/giskardpy/configs/iai_robots/ballbot.py
@@ -3,7 +3,7 @@ import numpy as np
 from giskardpy.configs.collision_avoidance_config import DisableCollisionAvoidanceConfig
 from giskardpy.configs.robot_interface_config import RobotInterfaceConfig, StandAloneRobotInterfaceConfig
 from giskardpy.configs.world_config import WorldWithFixedRobot
-from giskardpy.my_types import Derivatives
+from giskardpy.data_types import Derivatives
 
 
 class BallBotWorldConfig(WorldWithFixedRobot):
@@ -21,11 +21,9 @@ class BallBotCollisionAvoidanceConfig(DisableCollisionAvoidanceConfig):
 
 class BallBotJointTrajServerMujocoInterface(RobotInterfaceConfig):
     def setup(self):
-        self.sync_joint_state_topic('/joint_states')
+        self.sync_joint_state_topic('/ballbot/joint_states')
         self.add_follow_joint_trajectory_server(
-            namespace='/ballbot/follow_joint_trajectory',
-            state_topic='/ballbot/follow_joint_trajectory/state')
-
+            namespace='/ballbot/whole_body_controller')
 
 
 class BallBotStandAloneRobotInterfaceConfig(StandAloneRobotInterfaceConfig):
