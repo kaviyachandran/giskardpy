b0f89a8af3a5e1af70160d65df8423bbbe55a1ab
¨kaviyachandran¨ <kaviya.dhanabalachandran@gmail.com>
2024-08-13 17:09:57 +0200

    added closedloop with velocity controller

--- scripts/iai_robots/ballbot/ballbot_mujoco.py
+++ scripts/iai_robots/ballbot/ballbot_mujoco.py
@@ -2,12 +2,14 @@
 import rospy
 
 from giskardpy.configs.giskard import Giskard
+from giskardpy.configs.behavior_tree_config import ClosedLoopBTConfig
 from giskardpy.configs.iai_robots.ballbot import BallBotWorldConfig, BallBotCollisionAvoidanceConfig, \
-    BallBotJointTrajServerMujocoInterface
+    BallBotVelocityInterface
 
 if __name__ == '__main__':
     rospy.init_node('giskard')
     giskard = Giskard(world_config=BallBotWorldConfig(),
                       collision_avoidance_config=BallBotCollisionAvoidanceConfig(),
-                      robot_interface_config=BallBotJointTrajServerMujocoInterface())
+                      robot_interface_config=BallBotVelocityInterface(),
+                      behavior_tree_config=ClosedLoopBTConfig(debug_mode=True))
     giskard.live()
